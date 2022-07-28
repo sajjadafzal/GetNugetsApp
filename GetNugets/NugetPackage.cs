ab5986ceb86d1d89838eb916072e5d5aa900cf02
sajjadafzal <sajjadafzal@gmail.com>
2022-07-28 15:44:50 +0500

    Add project files.

---
+++ GetNugets/NugetPackage.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetNugets
+{
+    internal class NugetPackage : INotifyPropertyChanged
+    {
+        public string Package { get; set; }
+        public string Version { get; set; }
+        public bool Select { get; set; }
+
+        public string Error { get; set; }
+        public string Output { get; set; }
+
+        private bool exited;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public bool Exited 
+        { 
+            get => exited; 
+            set 
+            {
+                exited = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Exited)));
+            } 
+        }
+
+        public NugetPackage(string package, string version)
+        {
+            Package = package;
+            Version = version;
+            Select = true;
+            Exited = false;
+            Error = string.Empty;
+            Output = string.Empty;
+        }
+    }
+}
